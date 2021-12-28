e87a2e306d5adf1072632bcdf1768924ab6af8f2
medineannamammedova <medineannamammedova@Medine-MacBook-Pro.local>
2021-12-28 11:30:28 +0100

    commit

--- src/day01/Lambda02.java
+++ src/day01/Lambda02.java
@@ -1,14 +1,11 @@
 package day01;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class Lambda02 {
     public static void main(String[] args) {
 
-        List<Integer> list=new ArrayList<>(Arrays.asList(12, -3, 65, 3, 7, 34, 22, -60, 42, 15));
+        List<Integer> list = new ArrayList<>(Arrays.asList(12, -3, 65, 3, 7, 34, 22, -60, 42, 15));
 
         ciftKareMax(list);
         System.out.println();
@@ -42,54 +39,94 @@ public class Lambda02 {
         System.out.println();
         System.out.println("  ***  ");
 
+        onbesBkucukTekSayi(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        ciftKucBuy(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        tekKarBuyKuc(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+
     }
+
     //List'in cift olan elelmanlarin karelerini aliniz ve en buyugunu yazdiriniz
-    public static void ciftKareMax(List<Integer>list){
+    public static void ciftKareMax(List<Integer> list) {
         // Optional<Integer> maxEl = list.stream().filter(Lambda01::ciftBul).map(t-> t*t).reduce(Integer::max);
-        Optional<Integer> maxEl = list.stream().filter(Lambda01::ciftBul).map(t-> t*t).reduce(Math::max);
+        Optional<Integer> maxEl = list.stream().filter(Lambda01::ciftBul).map(t -> t * t).reduce(Math::max);
         System.out.println(maxEl);
     }
 
     //List'teki tum elemanlarin toplamini yazdiriniz.
     //LAmbda Expression...
-    public static void toplamEl(List<Integer>list) {
-        Optional<Integer>  toplam = list.stream().reduce(Integer::sum);
+    public static void toplamEl(List<Integer> list) {
+        Optional<Integer> toplam = list.stream().reduce(Integer::sum);
         System.out.println(toplam);
     }
+
     //List'teki cift elemanlarin carpimini yazdiriniz.
     //Method Reference...
-    public static void ciftCarp(List<Integer>list) {
+    public static void ciftCarp(List<Integer> list) {
         Optional<Integer> carp = list.stream().filter(Lambda01::ciftBul).reduce(Math::multiplyExact);
         System.out.println(carp);
     }
+
     //List'teki cift elemanlarin carpimini yazdiriniz.
     //Lambda expression...
-    public static void ciftCarp02(List<Integer>list) {
-        Integer carp = list.stream().filter(Lambda01::ciftBul).reduce(1,(x,y)->(x*y));
+    public static void ciftCarp02(List<Integer> list) {
+        Integer carp = list.stream().filter(Lambda01::ciftBul).reduce(1, (x, y) -> (x * y));
         System.out.println(carp);
     }
+
     //list'deki elemanlardan en kucugunu 4 farkli yontem ile yazdiriniz
     //1.yontem Method ile == Integer class'dan
-    public static void min1(List<Integer>list) {
+    public static void min1(List<Integer> list) {
         Optional<Integer> min = list.stream().reduce(Integer::min);
         System.out.println(min);
     }
+
     //2. yontem
-    public static void min2(List<Integer>list) {
+    public static void min2(List<Integer> list) {
         Optional<Integer> min = list.stream().reduce(Integer::min);
         System.out.println(min);
     }
-    //2.yontem method reference ile==Haluk class
+
+    //3.yontem method reference ile==Haluk class
     public static int minBul(int x, int y) {
-        return x<y ? x :y;//ternary
+        return x < y ? x : y;//ternary
     }
-    public static void min3(List<Integer>list) {
+
+    public static void min3(List<Integer> list) {
         Optional<Integer> min = list.stream().reduce(Lambda02::minBul);
         System.out.println(min);
     }
-    //3.yontem Method== Lambda Expression
-    public static void min4(List<Integer>list) {
-        Integer min = list.stream().reduce(Integer.MAX_VALUE, (x,y)-> x<y ? x:y);
+
+    //4.yontem Method== Lambda Expression
+    public static void min4(List<Integer> list) {
+        Integer min = list.stream().reduce(Integer.MAX_VALUE, (x, y) -> x < y ? x : y);
         System.out.println(min);
     }
+    //List'deki 15'ten buyuk en kucuk ve tek sayiyi yazdirin
+    public static void onbesBkucukTekSayi (List<Integer> list){
+        System.out.println(list.stream().filter(t-> t%2 ==1 & t>15).reduce(Integer::min));
+    }
+
+    //List'in cift elemanlarini kucukten buyuge yzdirim
+    public static void ciftKucBuy (List<Integer> list){
+        list.
+                stream().
+                filter(Lambda01:: ciftBul).
+                map(t-> t*t).
+                sorted().forEach(Lambda01::printEl);
+    }
+    //List'in tek elemanlarinin karelerini buyukten kucuge yazdirim
+    public static void tekKarBuyKuc(List<Integer> list){
+        list.stream().filter(t-> t%2!=0).map(t-> t*t).sorted(Comparator.reverseOrder()).forEach(Lambda01::printEl);
+    }
+
+
 }
---
+++ src/day01/Lamda03.java
@@ -0,0 +1,89 @@
+package day01;
+
+import java.util.*;
+
+public class Lamda03 {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>(Arrays.asList("Mehmet","Emre","Nilgun","Mehmet","Yildiz","Kader","Emine","Islam","Islam","Emret"));
+
+        buyHarfTeksiz(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        karakTeksizTersSira(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        karakSayKucBuy(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        sonHarfTers(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        uzunKareTeksizBuyKuc(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        karSay7az(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        wBaslamaKont(list);
+        System.out.println();
+        System.out.println("  ***  ");
+
+        karakBuy(list);
+        System.out.println();
+        System.out.println("  ***  ");
+    }
+   //List elemanlarini Alfabetik buyuk harf ve tekrarsiz yazdirin
+    public static void buyHarfTeksiz( List<String> list){
+        list.stream().map(t-> t.toUpperCase()).sorted().distinct().forEach(t-> System.out.print(t + " "));
+    }
+    //list elelmanlarinin character sayisini ters sirali olarak tekrarsiz yazdiriniz
+    public static void karakTeksizTersSira(List<String> list){
+        list.stream().map(t-> t.length()).sorted(Comparator.reverseOrder()).distinct().forEach(Lambda01::printEl);//string data type'i int'e cevirdik map'de
+    }
+ //List elemanlarini character sayisina gore kckten byk egore yazdiriniz.
+    public static void karakSayKucBuy(List<String> list){
+        list.stream().sorted(Comparator.comparing(t-> t.length())).forEach(t-> System.out.print(t + " "));
+    }
+    //List elemanlarinin son harfine gore ters sirali yazdirin
+    public static void sonHarfTers(List<String> list){
+        list.stream().sorted(Comparator.comparing(t-> t.toString().charAt(t.toString().length()-1)).reversed()).forEach(t-> System.out.print(t + " "));
+    }
+    //Cift sayili elemanlarin uzunluklarinin karelerini hesaplayan, tekrarsiz buyukten kucuge yazdiriniz
+    public static void uzunKareTeksizBuyKuc (List<String> list){
+        list.
+                stream().
+                map(t-> t.length()*t.length()).//string elemani karakter sayisina cevirildi
+                filter(Lambda01::ciftBul).
+                distinct().//tekrarsiz
+                sorted(Comparator.reverseOrder()).//ters sira b->k
+                forEach(t-> System.out.print(t + " "));
+    }
+    //List elemanlarinin karakter sayisini 7 ve 7'den az olma durumunu kontrol ediniz.
+    public static void karSay7az(List<String> list){
+      boolean kontrol= list.stream().allMatch(t-> t.length()<=7);
+     /* if (kontrol){
+          System.out.println("list elemanlari 7 harf'den buyuk degil.");
+      }else{
+          System.out.println("list elemanlari 7 harf'den buyuk.");
+      } if hali */
+        System.out.println(kontrol);
+    }
+    //List elemanlarinin "W" ile baslamasini kontrol edin
+    public static void wBaslamaKont(List<String> list){
+        System.out.println(list.stream().noneMatch(t-> t.startsWith("W")) ? "W ile basliyor" : "W ile baslamiyor");
+    }
+    //karaker sayisinin en buyuk elemanini yazdirin
+    public static void karakBuy(List<String> list){
+        System.out.println(list.
+                stream().
+                sorted(Comparator.comparing(t-> t.toString().length()).//length karakter uzunluguna gore siraladi
+                        reversed()).//ters siralama
+                findFirst());//ilk elemani aldi
+    }
+}
